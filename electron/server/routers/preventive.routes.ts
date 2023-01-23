53aa25461b1fb08bbb7700ee1e6e4d47cc25a3f2
leandrosantino <leandrosantino2013@gmail.com>
2023-01-23 14:47:27 -0300

    update

--- electron/server/routers/preventive.routes.ts
+++ electron/server/routers/preventive.routes.ts
@@ -1,9 +1,9 @@
 import { initTRPC } from '@trpc/server'
 import { z } from 'zod'
 import prisma from '../../services/prisma'
-import {internalServerError, successResponse,SuccessResponseSchema} from '../responseMessages'
+import { internalServerError, successResponse, SuccessResponseSchema } from '../responseMessages'
 
-import {weekYearRegex} from '../../utils/weekTools'
+import { weekYearRegex } from '../../utils/weekTools'
 import {
     assembleServiceOrders,
     registerServiceOrders,
@@ -15,36 +15,36 @@ import {
 const t = initTRPC.create()
 
 const actionsSchema = z.object({
-    id: z.number(),   
+    id: z.number().optional(),
     description: z.string(),
     machineId: z.number(),
-    excution : z.string(),
-    frequency : z.number(),
+    excution: z.string(),
+    frequency: z.number(),
     nextExecution: z.string().regex(weekYearRegex)
 })
 
 export const preventive = t.router({
-    
+
     assembleServiceOrders: t.procedure
         .input(z.object({
             year: z.number(),
             week: z.number()
         }))
         .output(z.array(serviceOrdersSchema))
-        .query(async ({input})=>{
+        .query(async ({ input }) => {
             try {
-                const OSs = await assembleServiceOrders(input.week, input.year )
+                const OSs = await assembleServiceOrders(input.week, input.year)
                 return OSs
             } catch (error) {
                 throw internalServerError(error)
             }
-        })    
+        })
     ,
 
     registerServiceOrders: t.procedure
         .input(serviceOrdersSchema)
         .output(SuccessResponseSchema)
-        .query(async ({input})=>{
+        .query(async ({ input }) => {
             try {
                 await registerServiceOrders(input)
                 return successResponse()
@@ -56,7 +56,8 @@ export const preventive = t.router({
 
     executeServiceOrders: t.procedure
         .input(executeServiceOrdersParamsSchema)
-        .query(async ({input})=>{
+        .output(SuccessResponseSchema)
+        .query(async ({ input }) => {
             try {
                 await executeServiceOrders(input)
                 return successResponse()
@@ -70,12 +71,29 @@ export const preventive = t.router({
         .output(z.array(actionsSchema))
         .query(async () => {
             try {
-                const actions = await prisma.preventiveAction.findMany() 
+                const actions = await prisma.preventiveAction.findMany()
                 return actions
             } catch (error) {
                 throw internalServerError(error)
             }
         })
     ,
-    
+
+    createActions: t.procedure
+        .input(actionsSchema)
+        .output(SuccessResponseSchema)
+        .query(async ({ input }) => {
+            try {
+                const action = await prisma.preventiveAction.create({
+                    data: {
+                        description: input.description,
+                    }
+                })
+
+                return successResponse()
+            } catch (error) {
+                throw internalServerError(error)
+            }
+        })
+
 })

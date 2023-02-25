336808a0ef6a291070389fd50cfa36eec25d1dc5
leandrosantino <76908036+leandrosantino@users.noreply.github.com>
2023-02-25 17:44:35 -0300

    Update Execute Service Orders Form

--- src/components/cards/PreventiveCard.tsx
+++ src/components/cards/PreventiveCard.tsx
@@ -4,7 +4,7 @@ import { VscDebugBreakpointLogUnverified, VscDebugBreakpointLog, VscSaveAs } fro
 import { BsPrinterFill } from 'react-icons/bs'
 import { InputButton } from '../forms/InputButton'
 import { ServiceOrderType } from '../../utils/schemas'
-import { api } from '../../utils/trpc'
+// import { api } from '../../utils/trpc'
 import { ipc } from '../../utils/ipc'
 import { usePages } from '../../hooks/usePages'
 import { splitWorkerName } from '../../utils/stringTools'
@@ -33,15 +33,15 @@ export function PreventiveCard({ data }: PreventiveCardProps) {
         w-full h-full p-2
         flex flex-warp flex-col
         justify-start items-center
-        shadow-md rounded-2xl 
-        bg-gray-400 
+        shadow-md rounded-2xl
+        bg-gray-400
         text-gray-900
       '
     >
       <header
         className={`
           flex flex-row justify-center items-center w-full px-2
-          border-b-2 ${data.concluded ? 'border-green-600' : 'border-gray-900'} font-medium text-lg 
+          border-b-2 ${data.concluded ? 'border-green-600' : 'border-gray-900'} font-medium text-lg
         `}
       >
         <div className='w-1/2 text-start py-2' >Nº {data.id}</div>
@@ -69,7 +69,7 @@ export function PreventiveCard({ data }: PreventiveCardProps) {
               key={index}
               className="
                 w-full pl-2
-                flex flex-row 
+                flex flex-row
                 justify-start items-center
               "
             >
@@ -98,7 +98,7 @@ export function PreventiveCard({ data }: PreventiveCardProps) {
         className="
           h-full w-full mt-2
           flex flex-col justify-end items-end
-          
+
         "
       >
         <div className='w-full flex flex-row justify-between items-center'>
@@ -118,7 +118,7 @@ export function PreventiveCard({ data }: PreventiveCardProps) {
           className={`
             w-full mt-2
             border-t ${data.concluded ? 'border-green-600' : 'border-gray-500'} p-2
-            flex flex-row justify-end 
+            flex flex-row justify-end
           `}
         >
 
--- src/pages/preventive/PreventivePlan/ExecuteServiceOrderForm.tsx
+++ src/pages/preventive/PreventivePlan/ExecuteServiceOrderForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { BiSave } from 'react-icons/bi'
 import { PageModalContainer } from '../../../components/containers/PageModalContainer'
 import { ScrollContainer } from '../../../components/containers/ScrollContainer'
@@ -9,11 +9,38 @@ import { usePages } from '../../../hooks/usePages'
 import { api } from '../../../utils/trpc'
 import { splitWorkerName } from '../../../utils/stringTools'
 
+function timeInStringToDate(hour: string) {
+  const [hours, minute] = hour.split(":").map(value => Number(value));
+  return new Date(0, 0, 0, hours, minute);
+}
+
+function dateInStringToDate(date: string) {
+  const [day, month, year] = date.split("-").map(value => Number(value));
+  return new Date(year, month - 1, day);
+}
+
+type ResponsableType = { id: number }
+
 export function ExecuteServiceOrderForm({ id }: { id: number }) {
 
-  const [registration, setRegistration] = useState<number>(0)
   const { data } = api.preventive.getServiceOrderById.useQuery({ id })
-  const worker = api.main.getWorkersByRegistration.useQuery(registration)
+  const [rep1, setResp1] = useState<ResponsableType>()
+  const [rep2, setResp2] = useState<ResponsableType>()
+  const [rep3, setResp3] = useState<ResponsableType>()
+  const [rep4, setResp4] = useState<ResponsableType>()
+
+  const [date, setDate] = useState<string>()
+  const [startTime, setStartTime] = useState<string>()
+  const [finishTime, setFinishTime] = useState<string>()
+
+
+  useEffect(() => {
+    console.log(date)
+    console.log(startTime)
+    console.log(finishTime)
+  }, [date, startTime, finishTime])
+
+
 
   const { backPage } = usePages()
 
@@ -38,45 +65,71 @@ export function ExecuteServiceOrderForm({ id }: { id: number }) {
           </div>
         </PageHeader>
 
-        <div className="flex flex-col justify-center items-center" >
+        <div className="flex flex-col justify-center items-center mt-[-10px]" >
 
           <div className="w-full grid grid-cols-3 gap-10 p-5">
             <InputCaseForm
               labelName='Data'
             >
-              <input className='w-full bg-transparent' type="date" />
-
+              <input
+                className='w-full bg-transparent'
+                type="date"
+                value={date}
+                onChange={e => setDate(e.target.value)}
+              />
             </InputCaseForm>
 
             <InputCaseForm
-              labelName='Tempo'
-            >
-              <input className='w-full bg-transparent' min={0} type="number" />
-              <span>min</span>
-            </InputCaseForm>
-
-            <InputCaseForm
-              labelName='Responsável'
+              labelName='Hora de Inicio'
             >
               <input
-                className='w-1/4 bg-transparent'
-                type="text"
-                onKeyUp={(e) => setRegistration(Number(e.currentTarget.value))}
+                className='w-full bg-transparent'
+                min={0}
+                type="time"
+                value={startTime}
+                onChange={e => setStartTime(e.target.value)}
               />
-              <span
-                className='w-3/4 text-end text-sm'
-              >{
-                  worker.isLoading ? 'Procurando...' :
-                    splitWorkerName(worker?.data?.name)
-                }</span>
             </InputCaseForm>
+
+            <InputCaseForm
+              labelName='Hora Final'
+            >
+              <input
+                className='w-full bg-transparent'
+                min={0}
+                type="time"
+                value={finishTime}
+                onChange={e => setFinishTime(e.target.value)}
+              />
+            </InputCaseForm>
+
+
+          </div>
+
+          <div className="w-full grid grid-cols-4 gap-10 p-5 mt-[-30px]">
+            <WorkerInput
+              labelName='Manutencista 1'
+              onChange={(id) => { if (id) setResp1({ id }) }}
+            />
+            <WorkerInput
+              labelName='Manutencista 2'
+              onChange={(id) => { if (id) setResp2({ id }) }}
+            />
+            <WorkerInput
+              labelName='Manutencista 3'
+              onChange={(id) => { if (id) setResp3({ id }) }}
+            />
+            <WorkerInput
+              labelName='Manutencista 4'
+              onChange={(id) => { if (id) setResp4({ id }) }}
+            />
           </div>
 
           <div className="w-full flex flex-col justify-center items-center" >
             <header
               className="w-full py-1 pl-1.5 font-medium text-xl"
             >Ações:</header>
-            <div className="w-full h-[calc(100vh-500px)] border-b border-gray-900">
+            <div className="w-full h-[calc(100vh-540px)] border-b border-gray-900">
               <ScrollContainer className="h-full px-2 py-2" >
                 <div
                   className="flex flex-col w-full p-0.5 "
@@ -111,3 +164,36 @@ export function ExecuteServiceOrderForm({ id }: { id: number }) {
   )
 }
 
+
+const WorkerInput = ({ onChange, labelName }: {
+  onChange(value: number | null | undefined): void,
+  labelName: string,
+}) => {
+
+  const [registration, setRegistration] = useState<number>(0)
+  const worker = api.main.getWorkersByRegistration.useQuery(registration)
+
+  return (
+    <InputCaseForm
+      labelName={labelName}
+    >
+      <input
+        className='w-1/4 bg-transparent'
+        type="text"
+        onChange={(e) => {
+          setRegistration(Number(e.target.value))
+          worker.refetch()
+            .then(worker => {
+              onChange(worker?.data?.id)
+            })
+        }}
+      />
+      <span
+        className='w-3/4 text-end text-sm'
+      >{
+          worker.isLoading ? 'Procurando...' :
+            splitWorkerName(worker?.data?.name)
+        }</span>
+    </InputCaseForm>
+  )
+}
